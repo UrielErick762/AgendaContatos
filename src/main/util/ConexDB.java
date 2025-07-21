a6f2fa4954efeec5e19070755f3933e75a9f283c
mafesilvapires <nanda.dasilvapires@gmail.com>
2025-07-20 22:00:00 -0300

    adiciona conexao db

---
+++ src/main/util/ConexDB.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public static Connection conectar() throws ClassNotFoundException {
+        Connection conexao = null;
+
+        try {
+            // Carrega o driver JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Dados da conexão
+            String url = "jdbc:mysql://localhost:3306/hospital";
+            String usuario = "root";
+            String senha = "";
+
+            // Estabelece conexão
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão realizada com sucesso!");
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+        }
+
+        return conexao;
+    }
+}
